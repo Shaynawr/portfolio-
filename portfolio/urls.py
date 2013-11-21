1af939fe7804b2256962a5cf308db24c0c9fe5e6
shayna <shaynawr@gmail.com>
2013-11-21 14:17:13 -0700

    added a url pattern to send everything to index.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'portfolio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^about/', TemplateView.as_view(template_name='about.html')),
+    url(r'^contact/', TemplateView.as_view(template_name='contact.html')),
+    url(r'^projects/', TemplateView.as_view(template_name='projects.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
