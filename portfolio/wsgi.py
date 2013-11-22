2d839cf4d96ac236162c62313ee8e23dcdbc5a1d
shayna <shaynawr@gmail.com>
2013-11-22 09:19:54 -0700

    add so that setting would load correctly

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'portfolio'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
